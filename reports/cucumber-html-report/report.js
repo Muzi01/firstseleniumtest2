1588f406e70901a0111fc9ebe05a1589b08d598c
Pior Kramkowski <piotr.kramkowski@ipfdigital.com>
2020-04-05 12:59:31 +0200

    Chages prior set up new - add fature and runners

---
+++ reports/cucumber-html-report/report.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("basket.feature");
+formatter.feature({
+  "line": 2,
+  "name": "basket",
+  "description": "",
+  "id": "basket",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@run"
+    }
+  ]
+});
+formatter.scenario({
+  "comments": [
+    {
+      "line": 3,
+      "value": "#shopping basket features"
+    }
+  ],
+  "line": 5,
+  "name": "add to basket",
+  "description": "",
+  "id": "basket;add-to-basket",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 6,
+  "name": "that i am on the shopping website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "i add an item to the basket",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 8,
+  "name": "i can view the item in my basket",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Basket.that_i_am_on_the_shopping_website()"
+});
+formatter.result({
+  "duration": 122263541,
+  "error_message": "java.lang.IllegalStateException: The path to the driver executable must be set by the webdriver.chrome.driver system property; for more information, see https://github.com/SeleniumHQ/selenium/wiki/ChromeDriver. The latest version can be downloaded from http://chromedriver.storage.googleapis.com/index.html\n\tat com.google.common.base.Preconditions.checkState(Preconditions.java:847)\n\tat org.openqa.selenium.remote.service.DriverService.findExecutable(DriverService.java:124)\n\tat org.openqa.selenium.chrome.ChromeDriverService.access$000(ChromeDriverService.java:32)\n\tat org.openqa.selenium.chrome.ChromeDriverService$Builder.findDefaultExecutable(ChromeDriverService.java:137)\n\tat org.openqa.selenium.remote.service.DriverService$Builder.build(DriverService.java:339)\n\tat org.openqa.selenium.chrome.ChromeDriverService.createDefaultService(ChromeDriverService.java:88)\n\tat org.openqa.selenium.chrome.ChromeDriver.\u003cinit\u003e(ChromeDriver.java:123)\n\tat bindings.Basket.that_i_am_on_the_shopping_website(Basket.java:22)\n\tat ✽.Given that i am on the shopping website(basket.feature:6)\n",
+  "status": "failed"
+});
+formatter.match({
+  "location": "Basket.i_add_an_item_to_the_basket()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "Basket.i_can_view_the_item_in_my_basket()"
+});
+formatter.result({
+  "status": "skipped"
+});
+});
