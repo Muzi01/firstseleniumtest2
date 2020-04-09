5acc4ee0dc06c68d687c30e8ffccb0bd68170a65
Pior Kramkowski <piotr.kramkowski@ipfdigital.com>
2020-04-09 09:48:10 +0200

    ad fix  to webdriver

---
+++ src/test/java/com/swtestacademy/webdriver/IdentifyingBrokenLinks.java
@@ -0,0 +1,55 @@
+package com.swtestacademy.webdriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class IdentifyingBrokenLinks {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void printBrokenLinks() {
+
+        driver.get("https://www.google.pl");
+        List<WebElement> anchor_links = driver.findElements(By.tagName("g"));
+
+        //getting the urls of the links into another List
+        List<String> link_urls = null;
+        for (WebElement link : anchor_links) {
+            //getting the status code of each link using GET method
+            int statusCode = given().get(link.getAttribute("href")).getStatusCode();
+            //verifying the status code
+            if (statusCode != 200) {
+                System.out.println(link.getText() + "-----" + "is broken");
+                System.out.println("The status code of broken link is: "+given().get(link.getAttribute("href")).getStatusCode());
+            }
+        }
+
+    }
+
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
