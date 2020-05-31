4ae516b19c8b7e6d0958c5c5e7a4b9d319d06368
piotr <piotr.kramkowski@gmail.com>
2020-05-31 13:57:47 +0200

    hotfix

--- src/test/java/tests/Tauron/Tests/BaseTauron.java
+++ src/test/java/tests/Tauron/Tests/BaseTauron.java
@@ -1,11 +1,12 @@
 package tests.Tauron.Tests;
 
+import bindings.cucumber.pages.Tauron.Pages.LoginPage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 import org.springframework.stereotype.Service;
 @Service
-public class BaseTauron extends tests.Tauron.Pages.LoginPage {
+public class BaseTauron extends bindings.cucumber.pages.Tauron.Pages.LoginPage {
     private static final Logger LOGGER = LogManager.getLogger(BaseTauron.class);
     public Object LoginPage;
 
