8715d0b0a797c50ba52b8c522f010b35803c1f17
piotr <piotr.kramkowski@gmail.com>
2020-05-31 12:52:41 +0200

    small changes

--- src/test/java/Orange/Tests/BaseOrange.java
+++ src/test/java/Orange/Tests/BaseOrange.java
@@ -1,10 +1,11 @@
 package Orange.Tests;
 
+import bindings.cucumber.pages.orange.LoginPage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 
-public class BaseOrange extends Orange.Pages.LoginPage {
+public class BaseOrange extends bindings.cucumber.pages.orange.LoginPage {
     private static final Logger LOGGER = LogManager.getLogger(BaseOrange.class);
     public Object LoginPage;
     private Object page;
