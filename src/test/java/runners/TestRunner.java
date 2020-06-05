4a686b131dae21c1cc4e68c85520f1a5715ba1b6
piotr <piotr.kramkowski@gmail.com>
2020-06-05 20:55:43 +0200

    fixes

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -2,17 +2,25 @@ package runners;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.junit.runner.RunWith;
+import org.testng.annotations.DataProvider;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources",
         glue= {"bindings"},
         plugin = { "pretty" },
-        tags = {"@links"},
+        tags = {"@login"},
         monochrome = true
 )
 
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @DataProvider (parallel = true)
+
+    public Object [][] scenarios () {
+        return super.scenarios();
+    }
 
 }
--- target/test-classes/login2.feature
+++ target/test-classes/login2.feature
@@ -1,9 +1,9 @@
 Feature: login orange
-  @1
+  @1 @login
   Scenario: Login To Orange
     When Customer is on application page
     And He is login to System
-    @3
+    @3 @login
     Scenario: Login to Orange Alternative
       When  Customer is on application page
       And  Fill His username
